ca9ea99965609ce4b65385df688906cfe2b6a938
chenkee <cchenkee@gmail.com>
2024-11-07 23:36:10 +0800

    Fix time zone and SSH connection environment variable loss issue

--- build/init_container.sh
+++ build/init_container.sh
@@ -2,10 +2,8 @@
 
 if [ -n "$SSH_PORT" ]; then
   sed -i "s/#Port 22/Port $SSH_PORT/" /etc/ssh/sshd_config
-  echo "SSH Port is be setted $SSH_PORT"
-  echo "export SSH_PORT=$SSH_PORT" >> /etc/profile
-  JUPYTER_PORT=$((8000 + SSH_PORT))
-  nohup jupyter lab --allow-root --no-browser --port=$JUPYTER_PORT --ip=0.0.0.0 --IdentityProvider.token='' --ServerApp.password='' --notebook-dir='/root' > /dev/null 2>&1 &
 fi
+# TODO: move it to Dockerfile
+echo 'for item in $(cat /proc/1/environ | tr "\0" "\n"); do export $item; done' >> /etc/profile
 
 /usr/sbin/sshd -D
--- run_container.sh
+++ run_container.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# 参数检查
+# arguments check
 if [ -z "$1" ]; then
-  echo "请传递容器名参数 格式为: bash run_container.sh <name>"
+  echo "请传递容器名(deep-<name>) 格式为: bash run_container.sh <name>"
   exit 1
 fi
 
 NAME=$1
 FILE="containers.txt"
 
-# 判断文件是否存在，如果不存在则创建
+# Check if the file exists, if not, create it
 if [ ! -f "$FILE" ]; then
     touch "$FILE"
-    echo "文件 $FILE 不存在，已创建。"
+    echo "端口管理文件 $FILE 不存在，正在创建。"
 fi
 
-# 在文件中搜索名字对应的端口号
+# Search for the port number corresponding to the name in the file
 PORT=$(grep "^$NAME " $FILE | awk '{print $2}')
 
 if [ -n "$PORT" ]; then
-  # 如果找到了名字对应的端口号
-  echo "该容器名存在对应端口号: $PORT"
+  # If the port number corresponding to the name is found
+  echo "端口管理文件中存在该容器名历史记录，将使用对应端口号: $PORT。"
 else
-  # 如果没有找到对应的名字，检查端口号是否递增
-  echo "容器名未找到，正在检查端口号递增情况..."
+  # If the corresponding name is not found, check if the port number is incrementing
+  echo "未在端口管理文件中找到该容器名，正在检查端口管理文件端口号递增情况..."
   PORTS=$(awk '{print $2}' $FILE | sort -n)
   
-  # 检查是否从23开始递增
+  # Check if it increments from 23
   EXPECTED_PORT=23
   MISSING_PORT=""
 
@@ -39,12 +39,12 @@ else
   done
 
   if [ -z "$MISSING_PORT" ]; then
-    # 没有缺失端口，添加新的端口
+    # No missing port, add a new port
     PORT=$EXPECTED_PORT
     echo "$NAME $PORT" >> $FILE
     echo "未找到缺失端口，将添加新的端口: $PORT"
   else
-    # 有缺失端口，补充缺失的端口号
+    # Missing port found, use the missing port number
      PORT=$MISSING_PORT
     echo "$NAME $MISSING_PORT" >> $FILE
     echo "存在缺失端口，将使用该端口号: $PORT"
@@ -52,44 +52,43 @@ else
 fi
 
 
-# 工作空间目录前缀
+# Workspace directory prefix
 WORKSPACE_DIR_PREFIX="/data"
-# Docker镜像名称
-DOCKER_IMAGE="cuda:12.4.1-cudnn-devel-miniconda-jupyter-ubuntu22.04"
+# Docker image name
+DOCKER_IMAGE="cuda:12.4.1-cudnn-miniconda-ubuntu22.04"
 
 
-# 定义工作目录
+# Define workspace directories
 WORKSPACE_DIR="${WORKSPACE_DIR_PREFIX}/${NAME}"
 WORKSPACE_SHARE_DIR="${WORKSPACE_DIR_PREFIX}/share/${NAME}"
 WORKSPACE_PUB_DIR="${WORKSPACE_DIR_PREFIX}/share/public"
-# 创建挂载目录
+# Create mount directories
 mkdir -p "${WORKSPACE_DIR}"
 mkdir -p "${WORKSPACE_SHARE_DIR}"
-mkdir -p "${WORKSPACE_DIR}/workspace"
-# Docker启动命令
+# Docker run command
 docker run -itd --gpus all --name "deep-${NAME}" --net=host -e SSH_PORT="${PORT}" -v "${WORKSPACE_DIR}":/root/workspace -v "${WORKSPACE_SHARE_DIR}":/root/share -v "${WORKSPACE_PUB_DIR}":/root/public ${DOCKER_IMAGE}
 
-# 检查端口是否已经开放
+# Check if the port is already open
 if firewall-cmd --zone=public --query-port="${PORT}/tcp"; then
   echo "端口 ${PORT}/tcp 已经开放，跳过防火墙设置"
 else
-  # 如果端口没有开放，则添加到防火墙并重载
+  # If the port is not open, add it to the firewall and reload
   firewall-cmd --zone=public --add-port="${PORT}/tcp" --permanent
   firewall-cmd --reload
   echo "已为端口 ${PORT}/tcp 添加防火墙规则并重新加载防火墙"
 fi
 
-# 检查端口 PORT+8000 是否已经开放
+# Check if the port PORT+8000 is already open
 JUPYTER_PORT=$((PORT + 8000))
 if firewall-cmd --zone=public --query-port="${JUPYTER_PORT}/tcp"; then
   echo "端口 ${JUPYTER_PORT}/tcp 已经开放，跳过防火墙设置"
 else
-  # 如果端口没有开放，则添加到防火墙并重载
+  # If the port is not open, add it to the firewall and reload
   firewall-cmd --zone=public --add-port="${JUPYTER_PORT}/tcp" --permanent
   firewall-cmd --reload
   echo "已为端口 ${JUPYTER_PORT}/tcp 添加防火墙规则并重新加载防火墙"
 fi
 
-# 输出容器端口号
+# Output container port number
 echo "容器已启动，端口号为: $PORT"
 echo "Jupyter Notebook端口号为: $JUPYTER_PORT"
